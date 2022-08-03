aa5acf631da902b4bde43b52afb71277be6f6ca5
Emil <e.garaev.dg@gmail.com>
2022-08-03 11:13:06 +0300

    combined reducers

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,27 +2,27 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
-
+import {createSelector} from "reselect";
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
-
 import './heroesList.scss';
 
-// Задача для этого компонента:
-// При клике на "крестик" идет удаление персонажа из общего состояния
-// Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
-
 const HeroesList = () => {
-    const filteredHeroes = useSelector(state => {
-        if(state.activeFilter === 'all'){
-            return state.heroes;
-        } else {
-            return state.heroes.filter(item => item.element === state.activeFilter)
+
+    const filteredHeroesSelector = createSelector(
+        (state) => state.filters.activeFilter,
+        (state) => state.heroes.heroes,
+        (filter, heroes) => {
+            if (filter === 'all') {
+                return heroes;
+            } else {
+                return heroes.filter(item => item.element === filter);
+            }
         }
-    })
-    const  heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
+    );
+    const filteredHeroes = useSelector(filteredHeroesSelector)
+    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
@@ -32,19 +32,14 @@ const HeroesList = () => {
             .then(data => dispatch(heroesFetched(data)))
             .catch(() => dispatch(heroesFetchingError()))
 
-        // eslint-disable-next-line
     }, []);
 
-    // Функция берет id и по нему удаляет ненужного персонажа из store
-    // ТОЛЬКО если запрос на удаление прошел успешно
-    // Отслеживайте цепочку действий actions => reducers
+
     const onDelete = useCallback((id) => {
-        // Удаление персонажа по его id
         request(`http://localhost:3001/heroes/${id}`, "DELETE")
             .then(data => console.log(data, 'Deleted'))
-            .then(dispatch(heroDeleted(id)))
+            .then(() => dispatch(heroDeleted(id)))
             .catch(err => console.log(err));
-        // eslint-disable-next-line  
     }, [request]);
 
     if (heroesLoadingStatus === "loading") {
